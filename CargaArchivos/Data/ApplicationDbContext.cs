0a9c8a949482f7b2b464d38c778de66e27ae7f15
JESUS EDUARDO <j_edi_24@hotmail.com>
2022-08-10 15:37:16 -0500

    agregamos todos los archivos

---
+++ CargaArchivos/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CargaArchivos.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CargaArchivos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+      
+    }
+}
